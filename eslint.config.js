d6ba4d263b20de13eea101467c19ebece14b7a68
Matthew McEachen <matthew@photostructure.com>
2024-11-18 08:40:05 -0800

    fix eslint for console commands

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,12 @@ import ts_eslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["src/**/*.ts", "scripts/*.js", "*.js"] },
+  {
+    files: ["src/**/*.ts", "scripts/*.js", "*.js"],
+    env: {
+      node: true // < enable Node.js global variables like `console`
+    }
+  },
   { ignores: [".tsup", "build", "coverage", "dist", "docs"] },
   pluginJs.configs.recommended,
   ...ts_eslint.configs.recommended,
