ad2c9b2f8806ec26e1ace65ee34f3b61070d0676
Matthew McEachen <matthew@photostructure.com>
2024-11-20 17:35:14 -0800

    feat: add remoteUser field to VolumeMetadata interface for remote access details

--- src/volume_metadata.ts
+++ src/volume_metadata.ts
@@ -40,6 +40,13 @@ export interface VolumeMetadata {
    */
   remote?: boolean;
   /**
+   * If remote, may include the username used to access the share.
+   *
+   * This will be undefined on NFS and other remote filesystem types that do
+   * authentication out of band.
+   */
+  remoteUser?: string;
+  /**
    * If remote, the ip or hostname hosting the share (like "rusty" or "10.1.1.3")
    */
   remoteHost?: string;
