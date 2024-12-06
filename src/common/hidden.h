4b950607c026ad241b0ee1b63fe82c826fe2c191
Matthew McEachen <matthew@photostructure.com>
2024-12-05 22:00:25 -0800

    add warning to Future Me

--- src/common/hidden.h
+++ src/common/hidden.h
@@ -3,6 +3,10 @@
 #include <napi.h>
 
 namespace FSMeta {
+// NOTE: LLMs want to declare Worker classes (like GetHiddenWorker) in the
+// header for some reason, but that isn't necessary.
+
 Napi::Promise GetHiddenAttribute(const Napi::CallbackInfo &info);
 Napi::Promise SetHiddenAttribute(const Napi::CallbackInfo &info);
+
 } // namespace FSMeta
