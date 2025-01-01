600168b083536f9d78385e7ee9699aceaa082cea
Matthew McEachen <matthew@photostructure.com>
2025-01-01 11:35:50 -0800

    fix: update .npmignore to add typescript source and exclude test files and build directory; enable sourcemaps in tsup.config

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,4 +4,5 @@ export default defineConfig({
   outExtension: ({ format }) => ({
     js: format === "cjs" ? ".cjs" : ".mjs", // Use .cjs for CommonJS and .mjs for ESM
   }),
+  sourcemap: true,
 });
