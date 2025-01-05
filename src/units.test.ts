70039355ac7410bde3ccadf689aef1fe0b23ccaf
Matthew McEachen <matthew@photostructure.com>
2025-01-05 13:57:07 -0800

    feat: add unit tests for byte formatting and enhance unit definitions

---
+++ src/units.test.ts
@@ -0,0 +1,41 @@
+import { GiB, KiB, MiB, TiB, fmtBytes } from "./units.js";
+
+describe("units", () => {
+  describe("fmtBytes", () => {
+    it("should format bytes", () => {
+      expect(fmtBytes(0)).toBe("0 B");
+      expect(fmtBytes(512)).toBe("512 B");
+      expect(fmtBytes(1023)).toBe("1023 B");
+    });
+
+    it("should format KiB", () => {
+      expect(fmtBytes(1023.5)).toBe("1.00 KiB");
+      expect(fmtBytes(KiB)).toBe("1.00 KiB");
+      expect(fmtBytes(1.5 * KiB)).toBe("1.50 KiB");
+      expect(fmtBytes(1023.994 * KiB)).toBe("1023.99 KiB");
+    });
+
+    it("should format MiB", () => {
+      expect(fmtBytes(1023.995 * KiB)).toBe("1.00 MiB");
+      expect(fmtBytes(MiB)).toBe("1.00 MiB");
+      expect(fmtBytes(1.5 * MiB)).toBe("1.50 MiB");
+      expect(fmtBytes(1023.98 * MiB)).toBe("1023.98 MiB");
+      expect(fmtBytes(1023.994 * MiB)).toBe("1023.99 MiB");
+    });
+
+    it("should format GiB", () => {
+      expect(fmtBytes(1023.995 * MiB)).toBe("1.00 GiB");
+      expect(fmtBytes(GiB)).toBe("1.00 GiB");
+      expect(fmtBytes(2.5 * GiB)).toBe("2.50 GiB");
+      expect(fmtBytes(15.784 * GiB)).toBe("15.78 GiB");
+      expect(fmtBytes(1023.994 * GiB)).toBe("1023.99 GiB");
+    });
+
+    it("should format TiB", () => {
+      expect(fmtBytes(1023.995 * GiB)).toBe("1.00 TiB");
+      expect(fmtBytes(1024 * GiB)).toBe("1.00 TiB");
+      expect(fmtBytes(2.5 * TiB)).toBe("2.50 TiB");
+      expect(fmtBytes(15.784 * TiB)).toBe("15.78 TiB");
+    });
+  });
+});
--- src/units.ts
+++ src/units.ts
@@ -1,31 +1,43 @@
 // src/units.ts
 
 /**
- * KiB = 1024 bytes
+ * Kibibyte (KiB) = 1024 bytes
  * @see https://en.wikipedia.org/wiki/Kibibyte
  */
 export const KiB = 1024;
 
 /**
- * MiB = 1024 KiB
+ * Mebibyte (MiB) = 1024 KiB
  * @see https://en.wikipedia.org/wiki/Mebibyte
  */
 export const MiB = 1024 * KiB;
 
 /**
- * GiB = 1024 MiB
+ * Gibibyte (GiB)= 1024 MiB
  * @see https://en.wikipedia.org/wiki/Gibibyte
  */
 export const GiB = 1024 * MiB;
 
+/**
+ * Tebibyte (TiB) = 1024 GiB
+ *
+ * @see https://en.wikipedia.org/wiki/Byte#Multiple-byte_units
+ */
+export const TiB = 1024 * GiB;
+
+const f = 1023.995 / 1024;
+
 export function fmtBytes(bytes: number): string {
-  if (bytes < KiB) {
+  if (bytes < 1023.5) {
+    bytes = Math.round(bytes);
     return `${bytes} B`;
-  } else if (bytes < MiB) {
+  } else if (bytes < MiB * f) {
     return `${(bytes / KiB).toFixed(2)} KiB`;
-  } else if (bytes < GiB) {
+  } else if (bytes < GiB * f) {
     return `${(bytes / MiB).toFixed(2)} MiB`;
-  } else {
+  } else if (bytes < TiB * f) {
     return `${(bytes / GiB).toFixed(2)} GiB`;
+  } else {
+    return `${(bytes / TiB).toFixed(2)} TiB`;
   }
 }
