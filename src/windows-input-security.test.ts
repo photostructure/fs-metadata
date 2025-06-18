c8c270f4b64f71f54742af6e3db16517256e770f
Matthew McEachen <matthew@photostructure.com>
2025-06-17 20:14:55 -0700

    fix(tests): update module imports to use node: prefix for consistency

--- src/windows-input-security.test.ts
+++ src/windows-input-security.test.ts
@@ -1,7 +1,7 @@
 // src/windows-input-security.test.ts
-import * as fs from "fs";
-import * as os from "os";
-import * as path from "path";
+import * as fs from "node:fs";
+import * as os from "node:os";
+import * as path from "node:path";
 import { isHidden } from "./index";
 import { describePlatform } from "./test-utils/platform";
 
